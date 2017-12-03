654cacba22515cc51f3e38ae2fc527b0c9f4639b
Ruan Pingcheng <ruanpingcheng@gmail.com>
2017-12-03 17:51:00 +0800

    Remove samples

--- samples/triangle/include/Line.h
+++
@@ -1,8 +0,0 @@
-#ifndef HDF1F8256_19AF_43C5_B868_901465BD519B
-#define HDF1F8256_19AF_43C5_B868_901465BD519B
-
-#include "tlp/list/algo/TypeList.h"
-
-#define __line(...)   __type_list(__VA_ARGS__)
-
-#endif
--- samples/triangle/include/Points.h
+++
@@ -1,8 +0,0 @@
-#ifndef HD0E7D0DA_7583_4BC9_B7C2_58C3CF230F8F
-#define HD0E7D0DA_7583_4BC9_B7C2_58C3CF230F8F
-
-#include "tlp/list/algo/TypeList.h"
-
-#define __points(...)   __type_list(__VA_ARGS__)
-
-#endif
